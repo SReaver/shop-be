683e17b56790500a7a802b041e72f0733135cd02
Sergey Plotnikov <reaverz@yandex.ru>
2023-04-15 00:31:41 +0600

    Change schema of getting list

--- product-service/lambda/getProductList.js
+++ product-service/lambda/getProductList.js
@@ -2,14 +2,19 @@ import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "../libs/ddbDocClient.js";
 import { setRespose } from '../libs/utils.js'
 
-const params = {
-  TableName: "products",
-};
-
 export const getProductList = async () => {
 	try {
-		const data = await ddbDocClient.send(new ScanCommand(params));
-		return setRespose(200, data.Items);
+		const { Items: products } = await ddbDocClient.send(new ScanCommand({ TableName: "products" }));
+		const { Items: stocks } = await ddbDocClient.send(new ScanCommand({ TableName: "stocks" }));
+
+		for (const product of products) {
+			const findedProductInStock = stocks.find(stock => stock.product_id === product.id);
+			product.count = findedProductInStock.count;
+			if (!findedProductInStock) {
+				return setRespose(404, { message: 'Cannot find product in stock' })
+			}
+		}
+		return setRespose(200, products);
 	} catch (error) {
 		return setRespose(500, error.message);
 	}
