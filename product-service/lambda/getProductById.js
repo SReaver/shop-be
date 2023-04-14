225265c9cac137b21ab9177739e7d789697b50ba
Sergey Plotnikov <reaverz@yandex.ru>
2023-04-15 00:04:00 +0600

    Use documentClient

--- product-service/lambda/getProductById.js
+++ product-service/lambda/getProductById.js
@@ -1,51 +1,34 @@
-import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 import { validate as uuidValidate } from 'uuid';
-import { ddbClient } from "../ddbClient.js";
 
-const setRespose = (code, body) => ({
-	headers: {
-		"Access-Control-Allow-Credentials": "true",
-		"Access-Control-Allow-Origin": "*",
-		"Content-Type": "application/json",
-	},
-	statusCode: code,
-	body: JSON.stringify(body),
-	isBase64Encoded: false
-})
-
-const returnParams = (tableName, key, id) => (
-	{
-		TableName: tableName,
-		Key: {
-			[key]: { S: id }
-		}
-	}
-);
+import { ddbDocClient } from "../libs/ddbDocClient.js";
+import { setRespose, getSearchParams } from '../libs/utils.js'
 
 export const getProductById = async (event) => {
-	const { pathParameters: { id }} = event;
+	const { pathParameters: { id } } = event;
 	if (!uuidValidate(id)) {
-		return setRespose(404, {
-			message: `Incorrect id: ${id}`
+		return setRespose(400, {
+			message: `Incorrect format of id: ${id}`
 		})
 	}
 
-	const paramsProduct = returnParams('products', 'id', id);
-	const paramsStock = returnParams('stocks', 'product_id', id);
+	const paramsProduct = getSearchParams('products', 'id', id);
+	const paramsStock = getSearchParams('stocks', 'product_id', id);
 
 	return Promise.all([
-		ddbClient.send(new GetItemCommand(paramsProduct)),
-		ddbClient.send(new GetItemCommand(paramsStock))
+		ddbDocClient.send(new GetCommand(paramsProduct)),
+		ddbDocClient.send(new GetCommand(paramsStock))
 	])
 		.then(([productItem, stockItem]) => {
-			if (!productItem?.Item || !stockItem?.Item) {
+			const { Item: product } = productItem;
+			const { Item: stock } = stockItem;
+
+			if (!product || !stock) {
 				return setRespose(404, {
 					message: 'Product not found'
 				})
 			} else {
-				return setRespose(200, {
-					body: JSON.stringify(Object.assign(productItem.Item, stockItem.Item))
-				})
+				return setRespose(200, { ...product, count: stock.count })
 			}
 		})
 		.catch(err => {
--- product-service/lambda/getProductList.js
+++ product-service/lambda/getProductList.js
@@ -1,39 +1,16 @@
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
-import { ddbClient } from "../ddbClient.js";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { ddbDocClient } from "../libs/ddbDocClient.js";
+import { setRespose } from '../libs/utils.js'
 
-const headers = {
-	"Access-Control-Allow-Credentials": true,
-	"Access-Control-Allow-Origin": "*",
-	"Content-Type": "application/json",
-}
-
-export const params = {
-  // Specify which items in the results are returned.
-  // FilterExpression: "Subtitle = :topic AND Season = :s AND Episode = :e",
-  // // Define the expression attribute value, which are substitutes for the values you want to compare.
-  // ExpressionAttributeValues: {
-  //   ":topic": { S: "SubTitle2" },
-  //   ":s": { N: "1" },
-  //   ":e": { N: "2" },
-  // },
-  // // Set the projection expression, which the the attributes that you want.
-  // ProjectionExpression: "Season, Episode, Title, Subtitle",
+const params = {
   TableName: "products",
 };
 
 export const getProductList = async () => {
 	try {
-		const data = await ddbClient.send(new ScanCommand(params));
-		return {
-			headers,
-			statusCode: 200,
-			body: JSON.stringify(data.Items)
-		};
+		const data = await ddbDocClient.send(new ScanCommand(params));
+		return setRespose(200, data.Items);
 	} catch (error) {
-		return {
-			headers,
-			statusCode: 500,
-			body: JSON.stringify(error.message)
-		};
+		return setRespose(500, error.message);
 	}
 }
