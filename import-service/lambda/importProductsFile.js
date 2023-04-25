08d69f2bfefc15bf6a0eb21fdd20464bc8450b11
Sergey Plotnikov <reaverz@yandex.ru>
2023-04-25 12:38:37 +0600

    File upload works with presign url

--- import-service/lambda/importProductsFile.js
+++ import-service/lambda/importProductsFile.js
@@ -1,25 +1,35 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3RequestPresigner } from "@aws-sdk/s3-request-presigner";
+import { parseUrl } from "@aws-sdk/url-parser";
+import { formatUrl } from "@aws-sdk/util-format-url";
+import { Hash } from "@aws-sdk/hash-node";
+import { fromEnv } from "@aws-sdk/credential-providers";
+import { HttpRequest } from "@aws-sdk/protocol-http";
 import { setRespose } from '../utils.js'
 
-const createPresignedUrlWithClient = async ({ region, bucket, key }) => {
-	const client = new S3Client({ region });
-	const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-	return getSignedUrl(client, command, { expiresIn: 3600 });
+const createPresignedUrlWithoutClient = async ({ region, bucket, key }) => {
+	const url = parseUrl(`https://${bucket}.s3.${region}.amazonaws.com/${key}`);
+	const presigner = new S3RequestPresigner({
+		credentials: fromEnv(),
+		region,
+		sha256: Hash.bind(null, "sha256"),
+	});
+
+	const signedUrlObject = await presigner.presign(
+		new HttpRequest({ ...url, method: "PUT" })
+	);
+	return formatUrl(signedUrlObject);
 };
 
 export const importProductsFile = async event => {
-
 	const { queryStringParameters: { name } } = event;
-
 	try {
-		const clientUrl = await createPresignedUrlWithClient({
-      region: 'eu-west-1',
-      bucket: 'files-upload-for-macshop',
-      key: `uploaded/${name}`,
-    });
+		const noClientUrl = await createPresignedUrlWithoutClient({
+			region: 'eu-west-1',
+			bucket: 'files-upload-for-macshop',
+			key: `uploaded/${name}`,
+		});
 
-		return setRespose(200, clientUrl);
+		return setRespose(200, noClientUrl);
 	} catch (error) {
 		return setRespose(500, {
 			message: error.message
